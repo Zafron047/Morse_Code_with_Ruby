7a46ab4344e7b6d1c7a0136f3cc12022cdaf22e4
MARVIN <o.silverygold@gmail.com>
2023-08-23 17:26:47 +0300

    Add decode_char and decode_word methods

--- index.rb
+++ index.rb
@@ -6,3 +6,21 @@ MORSE_CODE = {
   "..-" => "U", "...-" => "V", ".--" => "W", "-..-" => "X", "-.--" => "Y",
   "--.." => "Z"
 }
+
+def decode_char(param)
+  return MORSE_CODE[param]
+end
+
+puts decode_char(".-")
+
+def decode_word(paramStr)
+  array_Str = paramStr.split(" ")
+  word = []
+  array_Str.each do |param|
+    word.push(decode_char(param))
+  end
+  return word.join("")
+end
+
+puts decode_word("-- -.--")
+
