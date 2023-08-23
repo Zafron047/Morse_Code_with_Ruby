9fd526b3cd1de12dce529f29d19a9b2716c2bf0b
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-08-23 20:45:44 +0600

    Fix rubocop errors

--- index.rb
+++ index.rb
@@ -1,39 +1,39 @@
 MORSE_CODE = {
-  ".-" => "A", "-..." => "B", "-.-." => "C", "-.." => "D", "." => "E",
-  "..-." => "F", "--." => "G", "...." => "H", ".." => "I", ".---" => "J",
-  "-.-" => "K", ".-.." => "L", "--" => "M", "-." => "N", "---" => "O",
-  ".--." => "P", "--.-" => "Q", ".-." => "R", "..." => "S", "-" => "T",
-  "..-" => "U", "...-" => "V", ".--" => "W", "-..-" => "X", "-.--" => "Y",
-  "--.." => "Z"
-}
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+  '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+  '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+  '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+  '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
 
 def decode_char(param)
-  return MORSE_CODE[param]
+  MORSE_CODE[param]
 end
 
-puts decode_char(".-")
+puts decode_char('.-')
 
-def decode_word(paramStr)
-  array_Str = paramStr.split(" ")
+def decode_word(param_str)
+  array_str = param_str.split
   word = []
-  array_Str.each do |param|
+  array_str.each do |param|
     word.push(decode_char(param))
   end
-  return word.join("")
+  word.join
 end
 
-puts decode_word("-- -.--")
+puts decode_word('-- -.--')
 
 def decode_message(param)
-  words = param.split("   ")
+  words = param.split('   ')
   decoded_words = words.map do |word|
-    characters = word.split(" ")
-    decoded_characters = characters.map { |char| MORSE_CODE[char] || "?" }
-    decoded_characters.join("")
+    characters = word.split
+    decoded_characters = characters.map { |char| MORSE_CODE[char] || '?' }
+    decoded_characters.join
   end
-  decoded_words.join(" ")
+  decoded_words.join(' ')
 end
 
-message = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
-puts decode_message("-- -.--   -. .- -- .")
+message = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
+puts decode_message('-- -.--   -. .- -- .')
 puts decode_message(message)
